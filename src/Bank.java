a87ad1fcf4a07c009a13d8a8c5504e98f94d1166
dejan-marlovic <dmarlovic83@gmail.com>
2025-01-10 11:35:30 +0100

    Added show account function

--- src/Bank.java
+++ src/Bank.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+
 import java.util.HashMap;
 
 public class Bank {
@@ -10,15 +10,20 @@ public class Bank {
         return ++accountCounter;
     }
 
-    static void createUser(String name, int pinNumber){
+    static int createUser(String name, int pinNumber){
         SavingsAccount savingsAccount = new SavingsAccount(generateAccountNumber());
         PayrollAccount payrollAccount = new PayrollAccount(generateAccountNumber());
         User user = new User(savingsAccount, payrollAccount, name, generateAccountNumber(), pinNumber );
         users.put(accountCounter,user);
+        return accountCounter;
     }
 
-    static void showAccounts(int accountNumber){
-
+    static void showUserAccounts(int accountNumber){
+        User user = users.get(accountNumber);
+        System.out.println("Savings account number: " +
+                user.getSavingsAccount().getAccountNumber());
+        System.out.println("Savings account balance:" +
+                user.getSavingsAccount().getBalance());
     }
 
 }
